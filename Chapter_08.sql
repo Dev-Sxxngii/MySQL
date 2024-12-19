7a71950f4c7d2a6c2f3d8d2c45c1598495234380
Dev-Sxxngii <kkss0610k@gmail.com>
2024-12-19 16:57:58 +0900

    저장

--- Chapter_08.sql
+++ Chapter_08.sql
@@ -1,3 +1,7 @@
+# market_db 호출
+use market_db;
+
+# hongong1 테이블 생성
 create table hongong1 (
 	toy_id int,
     toy_name char(4),
@@ -11,28 +15,33 @@ insert into hongong1
 insert into hongong1(toy_id, toy_name) 
 	values(2, '버즈');
     
-insert into hongong1(toy_name, age, toy_id) 
+insert into hongong1(toy_name, age, toy_id) /* 대입 순서 지정 가능 */
 	values('제시', 20, 3);
 
+# hongong2 테이블 생성
 create table hongong2 (
 	toy_id int auto_increment primary key, /* auto_increment & primary key 세트 */
     toy_name char(4),
     age int
 );
 
-insert into hongong2 values (null, '보핍', 25);
-insert into hongong2 values (null, '슬링키', 22);
-insert into hongong2 values (null, '렉스', 21);
-select * from hongong2;
+# insert | auto_increment
+insert into hongong2 values (null, '보핍', 25); /* auto_increment로 인해 null이어도 자동 값 할당 */
+insert into hongong2 values (null, '슬링키', 22); /* auto_increment로 인해 null이어도 자동 값 할당 */
+insert into hongong2 values (null, '렉스', 21); /* auto_increment로 인해 null이어도 자동 값 할당 */
+
+# hongong2 테이블 조회
+select * from hongong2; 
 
 # auto_increment 현재 값
 select last_insert_id();
 
-# alter
+# alter: auto_increment 기본 값 변경
 alter table hongong2 auto_increment = 100;
 insert into hongong2 values (null, '재남', 35);
 select * from hongong2;
 
+# hongong3 테이블 생성 
 create table hongong3(
 	toy_id int auto_increment primary key,
     toy_name char(4),
